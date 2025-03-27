445567dab501762cfb6d4c053e48e4d56bebe38b
nghiaomg <trungnghia.260716@gmail.com>
2025-03-27 07:36:10 +0700

    Feat: Lab4

--- Lab4/ex03/md5_hash.py
+++
@@ -1,9 +0,0 @@
-def left_rotate(value, shift):
-    return ((value << shift) | (value >> 32 (32 - shift))) & 0xFFFFFFFF
-def md5(message):
-    a = 0x7452301
-    b = 0xEFCDA889
-    c = 0x98BADCFE
-    d = 0x10325476
-
-    original_length = len(message)
